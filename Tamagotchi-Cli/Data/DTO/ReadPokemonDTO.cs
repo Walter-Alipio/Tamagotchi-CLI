65d52ff080ff673b2c45ce9aa897bc4a1aab3923
Walter <silvawalteralipio@gmail.com>
2023-01-16 16:47:51 -0300

    add ToString method to show attributes

--- Tamagotchi-Cli/Data/DTO/ReadPokemonDTO.cs
+++ Tamagotchi-Cli/Data/DTO/ReadPokemonDTO.cs
@@ -18,4 +18,34 @@ public class ReadPokemonDTO
     this.types = types;
   }
 
+  public override string ToString()
+  {
+    string skills = string.Empty;
+    if (abilities != null)
+    {
+      foreach (var skill in abilities)
+      {
+        skills += skill.ability!.name + " ";
+      }
+    }
+    string pokemonTypes = string.Empty;
+    if (types != null)
+    {
+      foreach (var type in types)
+      {
+        pokemonTypes += type.type!.name + " ";
+      }
+    }
+
+    string response = $"Nome: {name!.ToUpper()}\n" +
+                      $"Altura: {height}\n" +
+                      $"Peso: {weight}\n" +
+                      "Habilidades: " +
+                      skills.Trim().Replace(" ", ", ") + "\n" +
+                      "Tipo: " +
+                      pokemonTypes.Trim().Replace(" ", ", ")
+                       ;
+    return response;
+  }
+
 }
